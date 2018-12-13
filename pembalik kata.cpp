93d6c4404f08ae58cbf7146be64484c48224256d
Yanuar <45526227+Yanuarramadhan@users.noreply.github.com>
2018-12-13 16:50:25 +0700

    codingan

---
+++ pembalik kata.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+char a[100];
+int b,c;
+int main()
+{
+printf ("masukan kata =");
+gets (a);
+strrev(a);
+printf("kebalikan katanya =%s",a);
+}
